e5fac8bbc0a762fa16844d2f1501ee5f8de22f3f
Panchagiri <panchagiri@Panchagiris-MBP.lan>
2022-08-26 19:41:43 -0500

    updated-issue

--- src/app/profile-page/profile-page.component.spec.ts
+++ src/app/profile-page/profile-page.component.spec.ts
@@ -43,7 +43,7 @@ describe('ProfilePageComponent', () => {
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.title')?.textContent).toEqual('Profile');
-    expect(component.profile.name).toEqual('King Julien');
+    expect(component.profile.name).toEqual('King Li Julien');
     expect(compiled.querySelector('.user-name')?.textContent).toEqual('King Julien');
     expect(component.profile.email).toEqual('kingj@email.com');
     expect(compiled.querySelector('.user-email')?.textContent).toEqual('kingj@email.com');
